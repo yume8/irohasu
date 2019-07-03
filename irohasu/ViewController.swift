a9d7f599791809e3790015caf2688a343994e425
yumeto <yumeto59@gmail.com>
2019-07-04 01:58:54 +0900

    Initial Commit

---
+++ irohasu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  irohasu
+//
+//  Created by 涌井夢人 on 2019/07/04.
+//  Copyright © 2019 涌井夢人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ irohasuTests/irohasuTests.swift
@@ -0,0 +1,34 @@
+//
+//  irohasuTests.swift
+//  irohasuTests
+//
+//  Created by 涌井夢人 on 2019/07/04.
+//  Copyright © 2019 涌井夢人. All rights reserved.
+//
+
+import XCTest
+@testable import irohasu
+
+class irohasuTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
